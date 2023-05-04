ff61c39c101bc0fdda459a321db7be3a5fc471b7
ijcrawford <ijcrawford@quinnipiac.edu>
2023-05-04 14:48:16 -0400

    fixing connection

--- backend/models/assignment.js
+++ backend/models/assignment.js
@@ -5,7 +5,7 @@ const assignmentSchema = new mongoose.Schema({
     assignmentContent: {
         title: {type: String, required: true},
         dueDate: {type: Date, required: true},
-        testCase: {type: File, required: true},
+        testCase: {type: String, required: true},
     },
     submission:[{
         studentId: {type: mongoose.Schema.Types.UUID},
--- backend/models/user.js
+++ backend/models/user.js
@@ -12,4 +12,4 @@ const userSchema = new mongoose.Schema({
 
 userSchema.plugin(passportLocalMongoose, {usernameField: "email"});
 
-module.exports = mongoose.model("student", userSchema);
+module.exports = mongoose.model("user", userSchema);
